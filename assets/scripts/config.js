71733eb8287bd09b95f0e33299aa454cd1b61dd3
MTaylor124 <matthew_taylor1@student.uml.edu>
2019-08-12 20:21:32 -0400

    Removed a single back slash

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://rocket-surveys.herokuapp.com/',
+  production: 'https://rocket-surveys.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
